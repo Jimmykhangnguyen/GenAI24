662dd440c2fa85a8f5f7a90d30b1a3c2982605ac
Jimmykhangnguyen <jimmykhangnguyen@gmail.com>
2024-03-30 16:32:54 -0400

    form

--- src/app.py
+++ src/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -6,6 +6,15 @@ app = Flask(__name__)
 @app.route('/home')
 def home():
     return render_template('home.html')
+    
+@app.route('/question', methods=['GET', 'POST'])
+def question():
+    if request.method == 'GET':
+        return render_template('question.html')
+    else:
+        have_charity = request.form['have_charity']
+        return render_template('q2.html', have_charity=have_charity)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
