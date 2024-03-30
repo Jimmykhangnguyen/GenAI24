d29a7c341551aa914eb43a4039f859419a1175ba
BambooShi <110565810+BambooShi@users.noreply.github.com>
2024-03-30 17:35:31 -0400

    modified forms

--- src/app.py
+++ src/app.py
@@ -13,11 +13,27 @@ def question():
         return render_template('question.html')
     else:
         have_charity = request.form['have_charity']
-        return render_template('q2.html', have_charity=have_charity)
-    
-@app.route('/q2')
-def q2():
-    return render_template('q2.html')
+        return render_template('question.html', have_charity=have_charity)
+
+@app.route('/charity_info', methods=['GET', 'POST'])
+def charity_info():
+    if request.method == 'GET':
+        return render_template('charity_info.html')
+    else:
+        charity_name = request.form['charity_name']
+        return render_template('charity_info.html', charity_name=charity_name)
+
+
+@app.route('/search_charities', methods=['GET', 'POST'])
+def search_charities():
+    if request.method == 'GET':
+        return render_template('search_charities.html')
+    else:
+        charity_type = request.form['charities']
+        charity_location = request.form['locations']
+
+        charity = [charity_type, charity_location]
+        return render_template('search_charities.html', charity=charity)
 
 
 if __name__ == '__main__':
